bf2625b719de03099563a46def5c668b8d34e186
decio2022 <113923924+decio2022@users.noreply.github.com>
2026-07-11 08:40:54 -0300

    add place to modFiles

--- js/mod.js
+++ js/mod.js
@@ -5,6 +5,7 @@ let modInfo = {
 	pointsName: "cash",
 	discordName: "",
 	discordLink: "",
+	modFiles: ["tree.js"]
 	initialStartPoints: new ExpantaNum (10), // Used for hard resets and new players
 	
 	offlineLimit: 1,  // In hours

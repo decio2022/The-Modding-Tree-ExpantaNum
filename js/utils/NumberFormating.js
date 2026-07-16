b0c8b3f59eeb54574f4e29d6708b359c1c77316d
decio2022 <113923924+decio2022@users.noreply.github.com>
2026-07-16 10:24:11 -0300

    Adding missing functions

--- js/utils/NumberFormating.js
+++ js/utils/NumberFormating.js
@@ -80,3 +80,16 @@ function toPlaces(x, precision, maxAccepted) {
     }
     return result
 }
+
+function formatSmall(x, precision=2) { 
+    return format(x, precision, true)    
+}
+
+function invertOOM(x){
+    let e = x.log10().ceil()
+    let m = x.div(Decimal.pow(10, e))
+    e = e.neg()
+    x = new ExpantaNum(10).pow(e).times(m)
+
+    return x
+}
